11e4a951223462b157da1ec7020938aa70f3f941
hungmt2426@gmail.com <hungmt2426@gmail.com>
2026-05-17 08:27:08 +0700

    add .env

--- services/order-service/src/main/java/com/nqh/orderservice/configurations/SecurityConfig.java
+++ services/order-service/src/main/java/com/nqh/orderservice/configurations/SecurityConfig.java
@@ -119,7 +119,9 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "http://192.168.88.136:5173"
+                "http://192.168.88.136:5173",
+                "http://10.53.68.83:5173",
+                "http://172.26.0.1:5173"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
